edd85fad46ed90989b35880aa2f4a1d31ac8bc55
Grégoire Seux <g.seux@criteo.com>
2021-12-02 09:06:21 +0100

    Solve day 2 🤿
    Change-Id: I8e5d7c3ab1630323cfaed3e172ed67c5648fc289

--- days/02.rb
+++ days/02.rb
@@ -2,13 +2,38 @@
 
 class Day02
   def initialize(input)
+    @depth = 0
+    @horizontal = 0
+    @aim = 0
+    @input = input
   end
 
   def run_part1
-    raise NotImplementedError
+    @input.split("\n").each do |line|
+      case line
+      when /forward (\d+)/
+        @horizontal += $1.to_i
+      when /down (\d+)/
+        @depth += $1.to_i
+      when /up (\d+)/
+        @depth -= $1.to_i
+      end
+    end
+    @depth * @horizontal
   end
 
   def run_part2
-    raise NotImplementedError
+    @input.split("\n").each do |line|
+      case line
+      when /forward (\d+)/
+        @horizontal += $1.to_i
+        @depth += @aim * $1.to_i
+      when /down (\d+)/
+        @aim += $1.to_i
+      when /up (\d+)/
+        @aim -= $1.to_i
+      end
+    end
+    @depth * @horizontal
   end
 end
