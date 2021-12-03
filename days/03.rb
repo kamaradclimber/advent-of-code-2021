c1f1425e31e1ddf842a6fdc667e3313a3e444997
Grégoire Seux <g.seux@criteo.com>
2021-12-03 09:08:57 +0100

    Solve day 3 🇬
    Change-Id: I03e80c2f62f414224337ba505cd372b5b691421e

--- days/03.rb
+++ days/03.rb
@@ -2,13 +2,45 @@
 
 class Day03
   def initialize(input)
+    @input = input.split("\n").map(&:chars)
   end
 
   def run_part1
-    raise NotImplementedError
+    gamma = @input.transpose.map do |column|
+      frequencies = column.group_by(&:itself).transform_values(&:size)
+      epsilon_bit = frequencies.min_by { |_, count| count }.first
+      gamma_bit = frequencies.max_by { |_, count| count }.first
+      # epsilon bit is the opposite of gamma_bit
+      gamma_bit
+    end.join.to_i(2)
+    epsilon = (2 ** @input.first.size - 1) ^ gamma
+    epsilon * gamma
   end
 
   def run_part2
-    raise NotImplementedError
+    oxygen_rating = rating(@input, 0) do |frequencies|
+      zeros = frequencies["0"]&.size || 0
+      ones = frequencies["1"]&.size || 0
+      ones >= zeros ? "1" : "0"
+    end.join.to_i(2)
+    co2_rating = rating(@input, 0) do |frequencies|
+      zeros = frequencies["0"]&.size || 0
+      ones = frequencies["1"]&.size || 0
+      ones >= zeros ? "0" : "1"
+    end.join.to_i(2)
+    oxygen_rating * co2_rating
+  end
+
+  # recursively select numbers until only one remains
+  # @param numbers [Array<Array<String>>]
+  def rating(numbers, index, &block)
+    return numbers.first if numbers.one?
+
+    frequencies = numbers.group_by do |number|
+      number[index]
+    end
+
+    bit_criteria = block.call(frequencies)
+    rating(frequencies[bit_criteria], index + 1, &block)
   end
 end
