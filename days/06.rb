46eea10e55edc3c6b42101fa569a9ebe9d28bbe9
Grégoire Seux <g.seux@criteo.com>
2021-12-06 08:57:13 +0100

    Solve day 6 - 🐡
    🐡 🐡 🐡
    🐡 🐡 🐡
    🐡 🐡 🐡
    🐡 🐡 🐡 🐡 🐡 🐡
    🐡 🐡 🐡 🐡 🐡 🐡
    🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡
    🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡
    🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡 🐡
    ...
    
    Change-Id: I39b6b809b6538b6d6e16cef76c101a5f2ab4af07

--- days/06.rb
+++ days/06.rb
@@ -2,13 +2,36 @@
 
 class Day06
   def initialize(input)
+    @population = [0] * 9
+    input.split(',').map(&:to_i).each do |lanternfish|
+      @population[lanternfish] += 1
+    end
   end
 
   def run_part1
-    raise NotImplementedError
+    80.times do |day|
+      tick
+    end
+    @population.sum
   end
 
   def run_part2
-    raise NotImplementedError
+    256.times do |day|
+      tick
+    end
+    @population.sum
+  end
+
+  def tick
+    new_population = [0] * 9
+    @population.each_with_index do |count, timer|
+      if timer == 0
+        new_population[6] += count
+        new_population[8] += count # new lanternfish
+      else
+        new_population[timer-1] += count
+      end
+    end
+    @population = new_population
   end
 end
