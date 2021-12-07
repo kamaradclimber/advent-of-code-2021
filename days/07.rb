3adc62a8d203e2fcce6b4f0b385d6a9ea0bbfa96
Grégoire Seux <kamaradclimber@gmail.com>
2021-12-07 08:40:34 +0100

    Solve day 7 - 🦀 ⛽
    Change-Id: I1ff5c23ece0ee65b98efcc0b3e040074da0c26a8

--- days/07.rb
+++ days/07.rb
@@ -2,13 +2,18 @@
 
 class Day07
   def initialize(input)
+    @positions = input.split(',').map(&:to_i)
   end
 
   def run_part1
-    raise NotImplementedError
+    (@positions.min..@positions.max).map do |pos|
+      @positions.map { |p| (pos - p).abs }.sum
+    end.min
   end
 
   def run_part2
-    raise NotImplementedError
+    (@positions.min..@positions.max).map do |pos|
+      @positions.map { |p| n = (pos - p).abs; n * (n+1)/2 }.sum
+    end.min
   end
 end
