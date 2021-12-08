a580bd706cb5abd146a63372ccc9b6f171ce7788
Grégoire Seux <kamaradclimber@gmail.com>
2021-12-08 07:49:22 +0100

    Solve day 08 - part1 🔢 🔄
    Change-Id: I15f54e1738fcfc6769d4b1b2f398c96c0c4fbb39

--- days/08.rb
+++ days/08.rb
@@ -2,13 +2,33 @@
 
 class Day08
   def initialize(input)
+    @lines = input.split("\n").map { |line| Line.parse(line) }
   end
 
   def run_part1
-    raise NotImplementedError
+    @lines.map do |line|
+      line.output_digits.count do |digit|
+        [2, 3, 4, 7].include?(digit.size)
+      end
+    end.sum
   end
 
   def run_part2
     raise NotImplementedError
+  end
+
+  class Line
+    def self.parse(line)
+      patterns, output = line.split("|").map(&:strip).map { |el| el.split(' ').map {|code| code.chars } }
+      Line.new(patterns, output)
+    end
+    def initialize(patterns, output_digits)
+      @patterns = patterns
+      @output_digits = output_digits
+    end
+    attr_reader :patterns, :output_digits
+
+
+
   end
 end
--- loop.sh
+++ loop.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-while inotifywait -e close_write days/* inputs/* spec/*; do rspec -fd --fail-fast; done
+while inotifywait -e close_write days/* inputs/* spec/*; do bundle exec rspec -fd --fail-fast; done
