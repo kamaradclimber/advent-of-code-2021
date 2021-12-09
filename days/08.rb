132e4525c651542382f8690fa8104081341c7f6f
Grégoire Seux <g.seux@criteo.com>
2021-12-09 09:47:36 +0100

    Solve day 08 part 2 🤯
    I canceled this automatic algorithm and used a hand crafted approach.
    
    Change-Id: I6b1f305e961738cfc995c1ca04bcc6391e8f3385
    Reference: https://www.reddit.com/r/adventofcode/comments/rc6sml/2021_day_8_decoding_the_multiverse

--- days/08.rb
+++ days/08.rb
@@ -14,94 +14,80 @@ class Day08
   end
 
   def run_part2
-    @lines = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf".split("\n").map { |line| Line.parse(line) }
-    @lines.each_with_index.map do |line, index|
-      puts "Solving line #{index}"
-      wires = ('a'..'g').map { |output_segment| [output_segment, Wire.new(output_segment)] }.to_h
-      line.patterns.each do |digit|
-        case digit.size
-        when 2
-          digit.each { |output_segment| wires[output_segment].restrict('cf'.chars) }
-        when 3
-          digit.each { |output_segment| wires[output_segment].restrict('acf'.chars) }
-        when 4
-          digit.each { |output_segment| wires[output_segment].restrict('bcdf'.chars) }
-        when 7
-          digit.each { |output_segment| wires[output_segment].restrict('abcdefg'.chars) }
-        end
-      end
+    @lines.map do |line|
+      association = {}
+      # segment f is used in all numbers except "2"
+      frequencies = line.patterns.flatten.group_by(&:itself).transform_values(&:size)
+      f_code = frequencies.find { |w, count| count == 9 }.first
+      raise "Unable to find what corresponds to segment f" unless f_code
 
-      unknown = wires.size
-      loop do
-        wires.values.each do |wire|
-          puts wire
-        end
-        wires.values.select(&:known?).each do |known_wire|
-          puts "We know the value of #{known_wire}"
-          wires.values.each do |wire|
-            next if wire.known?
+      # puts "When #{f_code} is light up, we are trying to light up f"
+      code_for_2 = line.patterns.find { |pattern| !pattern.include?(f_code)}
+      association[code_for_2] = 2
+      # puts "#{code_for_2.join} means we aimed to display 2"
 
-            puts "#{wire.output_segment} segment cannot be lighten up by #{known_wire.possible_input_segments}"
-            wire.reject(known_wire.possible_input_segments)
-          end
-        end
-        grouped_wires = wires.values.reject(&:known?).group_by(&:possible_input_segments)
-        self_sufficient_groups = grouped_wires.select { |possibles, g_wires| possibles.size == g_wires.size }
-        self_sufficient_groups.each do |possibles, g_wires|
-          puts "#{g_wires.map(&:output_segment).join(', ')} have the same possibilities (#{possibles.join(',')})"
-          wires.values.each do |wire|
-            next if g_wires.include?(wire)
-            puts "#{wire.output_segment} segment cannot be lighten up by #{possibles.join(' nor ')}"
-            wire.reject(possibles)
-          end
-        end
+      # now we detect the wires corresponding to "1" and wire for "c"
+      code_for_1 = line.patterns.select { |p| p.size == 2 }.first
+      c_code = (code_for_1 - [f_code]).first
+      # puts "When #{c_code} is light up, we are trying to light up c"
+      association[code_for_1] = 1
+      # puts "#{code_for_1.join} means we aimed to display 1"
 
-        break if wires.values.all?(&:known?)
-        new_unknown = wires.size - wires.values.count(&:known?)
-        puts "Before this iteration #{unknown} unknowns, now #{new_unknown}"
-        if unknown == new_unknown
+      # now we detect the wires usde for "7" and the wire for "a"
+      code_for_7 = line.patterns.select { |p| p.size == 3 }.first
+      a_code = (code_for_7 - [f_code, c_code]).first
+      # puts "When #{a_code} is light up, we are trying to light up a"
+      association[code_for_7] = 7
+      # puts "#{code_for_7.join} means we aimed to display 7"
+
+      # now we detect wires used for e
+      e_code = frequencies.find { |w, count| count == 4 }.first
+      raise "Unable to find what corresponds to segment e" unless e_code
+
+      # puts "When #{e_code} is light up, we are trying to light up e"
+
+      # now we detect wires used for b
+      b_code = frequencies.find { |w, count| count == 6 }.first
+      raise "Unable to find what corresponds to segment b" unless b_code
+
+      # puts "When #{b_code} is light up, we are trying to light up b"
+
+      # now we detect the wires usde for "4" and the wire for "d"
+      code_for_4 = line.patterns.select { |p| p.size == 4 }.first
+      d_code = (code_for_4 - [f_code, c_code, b_code]).first
+      # puts "When #{d_code} is light up, we are trying to light up d"
+      association[code_for_4] = 4
+      # puts "#{code_for_4.join} means we aimed to display 4"
+
+      # last unknown wire is g
+      g_code = (('a'..'g').to_a - [a_code, b_code, c_code, d_code, e_code, f_code]).first
+      # puts "When #{g_code} is light up, we are trying to light up g"
+
+      # missing numbers can now be deduced easily
+      association[[a_code, c_code, d_code, f_code, g_code]] = 3
+      association[[a_code, b_code, d_code, f_code, g_code]] = 5
+      association[[a_code, b_code, d_code, e_code, f_code, g_code]] = 6
+      association[[a_code, b_code, c_code, d_code, e_code, f_code, g_code]] = 8
+      association[[a_code, b_code, c_code, d_code, f_code, g_code]] = 9
+      association[[a_code, b_code, c_code, e_code, f_code, g_code]] = 0
+
+      association = association.map { |k,v| [k.sort, v]}.to_h
+
+      intended_output = line.output_digits.map do |digit_codes|
+        if !association.key?(digit_codes)
+          puts "#{digit_code} not found in association table"
           require 'pry'
           binding.pry
         end
-        unknown = new_unknown
-      end
-
-
-
-    end
-  end
-
-  class Wire
-    attr_reader :possible_input_segments, :output_segment
-
-    def initialize(output_segment)
-      @output_segment = output_segment
-      @possible_input_segments = 'abcdefg'.chars
-    end
-
-    def restrict(possible)
-      @possible_input_segments &= possible
-      return @possible_input_segments.first if @possible_input_segments.one?
-      raise "Impossible!" if @possible_input_segments.empty?
-      nil
-    end
-
-    def reject(impossible)
-      @possible_input_segments -= Array(impossible)
-    end
-
-    def known?
-      @possible_input_segments.one?
-    end
-
-    def to_s
-      "#{@possible_input_segments.join(', ')} -> #{@output_segment}"
-    end
+        association[digit_codes].to_s
+      end.join
+      intended_output.to_i
+    end.sum
   end
 
   class Line
     def self.parse(line)
-      patterns, output = line.split("|").map(&:strip).map { |el| el.split(' ').map {|code| code.chars } }
+      patterns, output = line.split("|").map(&:strip).map { |el| el.split(' ').map {|code| code.chars.sort } }
       Line.new(patterns, output)
     end
     def initialize(patterns, output_digits)
