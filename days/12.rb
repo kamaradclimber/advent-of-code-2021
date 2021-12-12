ebf0831a8087e11f1e229f2f63b8817226f61c40
Grégoire Seux <kamaradclimber@gmail.com>
2021-12-12 14:20:01 +0100

    Solve day 12 - part 2 🧭 🧵
    Change-Id: I0d993b4a35fd83bc760aae3ac3e7c66a367abaec

--- days/12.rb
+++ days/12.rb
@@ -11,6 +11,10 @@ class Day12
   end
 
   def run_part1
+    explore
+  end
+
+  def explore(part2 = false)
     complete_paths = []
     to_explore = []
     path = Path.new
@@ -19,7 +23,7 @@ class Day12
     while to_explore.any?
       path = to_explore.pop
       @edges[path.current_loc].each do |cave|
-        next if path.visited_small_caves.include?(cave)
+        next unless path.can_visit?(cave, part2)
 
         new_path = path.dup
         new_path << cave
@@ -34,18 +38,18 @@ class Day12
   end
 
   def run_part2
-    raise NotImplementedError
+    explore(true)
   end
 
   class Path
     def initialize(path = [])
-      @visited_small_caves = []
+      @visited_small_caves = Hash.new(0)
       @path = path
     end
 
     def <<(cave)
       @path << cave
-      @visited_small_caves << cave if cave.downcase == cave
+      @visited_small_caves[cave] += 1 if cave.downcase == cave
       self
     end
 
@@ -57,12 +61,24 @@ class Day12
       @path.last
     end
 
-    attr_accessor :visited_small_caves
+    def can_visit?(cave, part2)
+      return true if cave.upcase == cave
+      return true if cave == 'end'
+      return false if cave == 'start'
+
+      if part2
+        !@visited_small_caves.keys.include?(cave) || @visited_small_caves.none? { |_, c| c > 1 }
+      else
+        !@visited_small_caves.keys.include?(cave)
+      end
+    end
 
     def dup
       new_path = Path.new(@path.dup)
       new_path.visited_small_caves = visited_small_caves.dup
       new_path
     end
+
+    attr_accessor :visited_small_caves
   end
 end
